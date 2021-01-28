d7656753488ce4e89a8442ed29fee95efbab00bd
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-01-28 11:45:50 +0200

    Display courses and select course

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,13 @@
+import { Icu } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
+
+interface ICourse {
+  id: number;
+  title: string;
+  description: string;
+  percentComplete: number;
+  favorite: boolean;
+}
 
 @Component({
   selector: 'app-courses',
@@ -6,19 +15,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
-  courses = [
+  selectedCourse: ICourse = null;
+  courses: ICourse[] = [
     {
       id: 1,
       title: 'Angular 9 Fundamentals',
       description: 'Learn the fundamentals of Angular 9',
       percentComplete: 26,
       favorite: true
+    },
+    {
+      id: 2,
+      title: 'Javascript Fundamentals',
+      description: 'Learn the fundamentals of Javascript',
+      percentComplete: 30,
+      favorite: false
+    },
+    {
+      id: 3,
+      title: 'Component Fundamentals',
+      description:
+        'Learn the fundamentals of Angular components',
+      percentComplete: 55,
+      favorite: false
     }
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectCourse(course: ICourse) {
+    this.selectedCourse = course;
+  }
+
+  deleteCourse(id: number) {
+    // TODO: delete course
   }
 
 }
