c981423fb7c36b2f60f66379b2cc70f7d005f360
Alexandra Stoica <ralexandrastoica@gmail.com>
2021-01-28 17:38:57 +0200

    Add links to sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface ILink {
+  path: string;
+  icon: string;
+  title: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +13,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Workshop library';
+  links: ILink[] = [
+    { path: '/home', icon: 'home', title: 'Home' },
+    { path: '/courses', icon: 'view_list', title: 'Courses' },
+  ];
 
   constructor() {}
 }
